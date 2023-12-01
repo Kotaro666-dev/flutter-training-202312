a6b47498dbc74d5bf821db55509abfe078b98e0c
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2023-12-01 17:00:07 +0900

    issue #2 変更: diagnostic_describe_all_properties の Lint エラーに対応する

--- lib/presentation/weather/components/weather_temperature_display.dart
+++ lib/presentation/weather/components/weather_temperature_display.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 
 class WeatherTemperatureDisplay extends StatelessWidget {
   const WeatherTemperatureDisplay({
-    required this.minTemperature,
-    required this.maxTemperature,
+    required int minTemperature,
+    required int maxTemperature,
     super.key,
-  });
+  })  : _maxTemperature = maxTemperature,
+        _minTemperature = minTemperature;
 
-  final int minTemperature;
-  final int maxTemperature;
+  final int _minTemperature;
+  final int _maxTemperature;
 
   @override
   Widget build(BuildContext context) {
@@ -25,13 +26,13 @@ class WeatherTemperatureDisplay extends StatelessWidget {
             children: [
               Expanded(
                 child: TemperatureLabel(
-                  temperature: minTemperature,
+                  temperature: _minTemperature,
                   fontColor: Colors.blue,
                 ),
               ),
               Expanded(
                 child: TemperatureLabel(
-                  temperature: maxTemperature,
+                  temperature: _maxTemperature,
                   fontColor: Colors.red,
                 ),
               ),
@@ -45,23 +46,24 @@ class WeatherTemperatureDisplay extends StatelessWidget {
 
 class TemperatureLabel extends StatelessWidget {
   const TemperatureLabel({
-    required this.temperature,
-    required this.fontColor,
+    required int temperature,
+    required Color fontColor,
     super.key,
-  });
+  })  : _fontColor = fontColor,
+        _temperature = temperature;
 
-  final int temperature;
-  final Color fontColor;
+  final int _temperature;
+  final Color _fontColor;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final textStyle = theme.textTheme.labelLarge?.copyWith(
-          color: fontColor,
+          color: _fontColor,
         ) ??
-        TextStyle(color: fontColor);
+        TextStyle(color: _fontColor);
     return Text(
-      '$temperature ℃',
+      '$_temperature ℃',
       textAlign: TextAlign.center,
       style: textStyle,
     );
