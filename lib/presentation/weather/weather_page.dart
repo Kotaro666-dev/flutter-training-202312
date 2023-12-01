cb3462cc99a4c990f84097561ca87cdb7895f2e1
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2023-12-01 13:11:26 +0900

    issue #2 更新: 基本的なレイアウト構成を作成する

--- lib/presentation/weather/weather_page.dart
+++ lib/presentation/weather/weather_page.dart
@@ -17,7 +17,89 @@ class _Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text('Weather Page'),
+      child: FractionallySizedBox(
+        widthFactor: 0.5,
+        // Note: 画面を三分割して考えて、Middleの部分が中央に配置されるようにする
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Top
+            Spacer(),
+
+            // Middle（本ウィジェットが画面の中央に位置する）
+            WeatherAndTemperatures(),
+
+            // Bottom
+            Expanded(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 80,
+                  ),
+                  ActionButtons(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WeatherAndTemperatures extends StatelessWidget {
+  const WeatherAndTemperatures({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        AspectRatio(
+          aspectRatio: 1,
+          child: Placeholder(),
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: Text(
+                '** ℃',
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Expanded(
+              child: Text(
+                '** ℃',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class ActionButtons extends StatelessWidget {
+  const ActionButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Flexible(
+          child: TextButton(
+            onPressed: () {},
+            child: const Text('Close'),
+          ),
+        ),
+        Flexible(
+          child: TextButton(
+            onPressed: () {},
+            child: const Text('Reload'),
+          ),
+        ),
+      ],
     );
   }
 }
