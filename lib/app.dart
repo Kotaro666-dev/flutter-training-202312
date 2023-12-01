1542186b035ed2ad74227d60c17a49ee5730246e
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2023-12-01 12:15:59 +0900

    issue #2 変更: ディレクトリ構成をレイヤーごとに分類する方針にして、presentation 層に weather_page を移動する

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_training/weather_page.dart';
+import 'package:flutter_training/presentation/weather/weather_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
