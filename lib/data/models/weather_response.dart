4057a95ef427702c32acd7640015b87671090dcc
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2023-12-04 17:00:07 +0900

    issue #3 修正: Lint エラーに対応する

--- lib/data/models/weather_response.dart
+++ lib/data/models/weather_response.dart
@@ -1,4 +1,5 @@
-// TODO: Session7 #8 シリアル化する 対応時に json 型レスポンスから freezed で作成した WeatherResponse に変換する
+// TODO: Session7 #8 シリアル化する
+// 対応時に json 型レスポンスから freezed で作成した WeatherResponse に変換する
 class WeatherResponse {
   const WeatherResponse({
     required this.condition,
