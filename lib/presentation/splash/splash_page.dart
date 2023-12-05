abbd46e2108e1a9738c5fee1f1588d2261398a1a
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2023-12-05 11:19:29 +0900

    issue #4 更新: StatefulWidget で作成した緑の画面（アプリ内で SplashPage と命名する）を追加する

---
+++ lib/presentation/splash/splash_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const DecoratedBox(
+      decoration: BoxDecoration(
+        color: Colors.green,
+      ),
+    );
+  }
+}
