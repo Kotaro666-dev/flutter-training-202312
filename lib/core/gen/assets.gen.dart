21d9ed4bed9e59501a24345bc1a98009370a7874
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2023-12-05 10:24:25 +0900

    issue #3 変更: カスタム設定を基にして flutter_gen の生成ファイルを更新する

--- lib/core/gen/assets.gen.dart
+++ lib/core/gen/assets.gen.dart
@@ -8,21 +8,23 @@
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/services.dart';
 
 class $AssetsWeatherGen {
   const $AssetsWeatherGen();
 
   /// File path: assets/weather/cloudy.svg
-  String get cloudy => 'assets/weather/cloudy.svg';
+  SvgGenImage get cloudy => const SvgGenImage('assets/weather/cloudy.svg');
 
   /// File path: assets/weather/rainy.svg
-  String get rainy => 'assets/weather/rainy.svg';
+  SvgGenImage get rainy => const SvgGenImage('assets/weather/rainy.svg');
 
   /// File path: assets/weather/sunny.svg
-  String get sunny => 'assets/weather/sunny.svg';
+  SvgGenImage get sunny => const SvgGenImage('assets/weather/sunny.svg');
 
   /// List of all assets
-  List<String> get values => [cloudy, rainy, sunny];
+  List<SvgGenImage> get values => [cloudy, rainy, sunny];
 }
 
 class Assets {
@@ -97,6 +99,59 @@ class AssetGenImage {
       _assetName,
       bundle: bundle,
       package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName);
+
+  final String _assetName;
+
+  SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    SvgTheme theme = const SvgTheme(),
+    ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    return SvgPicture.asset(
+      _assetName,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      bundle: bundle,
+      package: package,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      theme: theme,
+      colorFilter: colorFilter,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
     );
   }
 
