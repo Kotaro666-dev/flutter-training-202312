495dbc58ba1406d100a35dd53fe83d16262325c5
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2023-12-05 16:07:59 +0900

    issue #4 更新: 生成した go_router_config.g,dart ファイルを追加する

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_training/presentation/splash/splash_page.dart';
-import 'package:flutter_training/presentation/weather/weather_page.dart';
+import 'package:flutter_training/core/router/go_router_config.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const SplashPage(),
-      routes: {
-        WeatherPage.routeName: (_) => const WeatherPage(),
-      },
+    return MaterialApp.router(
+      routerConfig: goRouterConfig,
     );
   }
 }
