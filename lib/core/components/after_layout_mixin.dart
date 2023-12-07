ad95404d918c8177bdc3668498ed59ea4ccf8fa9
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2023-12-07 12:44:51 +0900

    issue #5 更新: 画面の描写を待った後に何かしらのアクションが実行される Mixin を定義する

---
+++ lib/core/components/after_layout_mixin.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+mixin AfterLayoutMixin<T extends StatefulWidget> on State<T> {
+  @override
+  void initState() {
+    unawaited(doActioinAfterFrameCompletes());
+    super.initState();
+  }
+
+  Future<void> doActioinAfterFrameCompletes() async {
+    // 画面の描写が完了することを待つ
+    await WidgetsBinding.instance.endOfFrame;
+
+    // 画面の描写が完了した後に、onAfterLayoutを呼び出す
+    await onAfterLayout();
+  }
+
+  /// 画面の描写が完了した後に呼び出される
+  Future<void> onAfterLayout();
+}
