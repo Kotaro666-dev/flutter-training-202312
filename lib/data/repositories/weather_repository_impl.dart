ca350f91d92064a9d88a7b7aae76f5048b75fa2b
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2023-12-08 14:59:41 +0900

    issue #7 更新: リポジトリ層にレスポンスデータからドメイン層データに変換するメソッドを追加し、繋ぎこむ

--- lib/data/repositories/weather_repository_impl.dart
+++ lib/data/repositories/weather_repository_impl.dart
@@ -2,11 +2,14 @@ import 'dart:io';
 
 import 'package:flutter_training/core/exceptions/weather_exceptions.dart';
 import 'package:flutter_training/data/datasources/remote/weather_remote_data_source.dart';
+import 'package:flutter_training/data/models/weather_request.dart';
+import 'package:flutter_training/data/models/weather_response.dart';
 import 'package:flutter_training/domain/models/weather.dart';
 import 'package:flutter_training/domain/repositories/weather_repository.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
 const _sampleArea = 'tokyo';
+const _sampleDate = '2020-04-01T12:00:00+09:00';
 
 class WeatherRepositoryImpl implements WeatherRepository {
   WeatherRepositoryImpl({
@@ -18,10 +21,14 @@ class WeatherRepositoryImpl implements WeatherRepository {
   @override
   Future<Weather> fetchWeather() async {
     try {
-      // Note: area は固定値で渡しているが、本来はユーザーが選択した地域を渡す
-      final response =
-          await _weatherRemoteDataSource.fetchThrowsWeather(area: _sampleArea);
-      return Weather.fromName(response.condition);
+      // Note: リクエストパラメータは固定値で渡しているが、本来はユーザーが選択した値を渡す
+      final response = await _weatherRemoteDataSource.fetchWeather(
+        request: WeatherRequest(
+          area: _sampleArea,
+          date: _sampleDate,
+        ),
+      );
+      return _toWeather(response);
     } on YumemiWeatherError catch (e) {
       switch (e) {
         case YumemiWeatherError.invalidParameter:
@@ -29,10 +36,34 @@ class WeatherRepositoryImpl implements WeatherRepository {
         case YumemiWeatherError.unknown:
           throw UnknownWeatherException();
       }
+    } on FormatException catch (e) {
+      throw UndefinedWeatherException(message: e.toString());
     } on IOException catch (e) {
       throw NetworkException(message: e.toString());
     } on Exception catch (e) {
       throw UnexpectedException(message: e.toString());
     }
+  }
+
+  Weather _toWeather(WeatherResponse response) {
+    final isValid = [
+      response.weatherCondition,
+      response.maxTemperature,
+      response.minTemperature,
+      response.date,
+    ].every((e) => e != null);
+
+    if (!isValid) {
+      throw const FormatException('WeatherResponse is invalid');
+    }
+
+    final condition = WeatherCondition.from(response.weatherCondition!);
+    final date = DateTime.parse(response.date!);
+    return Weather(
+      condition: condition,
+      maxTemperature: response.maxTemperature!,
+      minTemperature: response.minTemperature!,
+      date: date,
+    );
   }
 }
