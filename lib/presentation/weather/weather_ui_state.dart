39d5a04a163acdb00cab895b2f78d322b4857335
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2023-12-08 15:48:22 +0900

    issue #7 更新: 天気画面の UiState を追加する

---
+++ lib/presentation/weather/weather_ui_state.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_training/domain/models/weather.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'weather_ui_state.freezed.dart';
+
+@freezed
+class WeatherUiState with _$WeatherUiState {
+  const factory WeatherUiState({
+    required String minTemperature,
+    required String maxTemperature,
+    WeatherCondition? condition,
+  }) = _WeatherUiState;
+}
