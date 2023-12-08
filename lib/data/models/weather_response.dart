aac9fa033a9d51f08a1063b53f9360c066b85d85
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2023-12-08 14:20:24 +0900

    issue #7 変更: YumemiWeather から取得するレスポンスデータを格納するクラスのパラメータに変更する

--- lib/data/models/weather_response.dart
+++ lib/data/models/weather_response.dart
@@ -2,14 +2,23 @@
 // 対応時に json 型レスポンスから freezed で作成した WeatherResponse に変換する
 class WeatherResponse {
   const WeatherResponse({
-    required this.condition,
+    required this.weatherCondition,
+    required this.maxTemperature,
+    required this.minTemperature,
+    required this.date,
   });
 
-  factory WeatherResponse.from({
-    required String response,
-  }) {
-    return WeatherResponse(condition: response);
+  factory WeatherResponse.fromJson(Map<String, dynamic> json) {
+    return WeatherResponse(
+      weatherCondition: json['weather_condition'] as String?,
+      maxTemperature: json['max_temperature'] as int?,
+      minTemperature: json['min_temperature'] as int?,
+      date: json['date'] as String?,
+    );
   }
 
-  final String condition;
+  final String? weatherCondition;
+  final int? maxTemperature;
+  final int? minTemperature;
+  final String? date;
 }
