bda13c2dd9dae7caf2b7c90cc03f6b0335201458
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2023-12-08 14:58:27 +0900

    issue #7 変更: ドメイン層 Weather クラスのメンバーを変更する

--- lib/domain/models/weather.dart
+++ lib/domain/models/weather.dart
@@ -1,16 +1,30 @@
 import 'package:flutter_training/core/exceptions/weather_exceptions.dart';
 
-enum Weather {
+class Weather {
+  const Weather({
+    required this.condition,
+    required this.maxTemperature,
+    required this.minTemperature,
+    required this.date,
+  });
+
+  final WeatherCondition condition;
+  final int maxTemperature;
+  final int minTemperature;
+  final DateTime date;
+}
+
+enum WeatherCondition {
   sunny,
   cloudy,
   rainy;
 
-  factory Weather.fromName(String name) {
-    for (final value in Weather.values) {
-      if (value.name == name) {
+  factory WeatherCondition.from(String condition) {
+    for (final value in WeatherCondition.values) {
+      if (value.name == condition) {
         return value;
       }
     }
-    throw UndefinedWeatherException(message: 'Undefined weather: $name');
+    throw UndefinedWeatherException(message: 'Undefined weather: $condition');
   }
 }
