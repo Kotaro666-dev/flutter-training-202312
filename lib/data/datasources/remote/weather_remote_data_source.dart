6707101c668b44cde271800f909c6c054463249e
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2023-12-11 16:45:06 +0900

    issue #7 削除: 不要な DataSource メソッドを削除する

--- lib/data/datasources/remote/weather_remote_data_source.dart
+++ lib/data/datasources/remote/weather_remote_data_source.dart
@@ -5,8 +5,6 @@ import 'package:flutter_training/data/models/weather_response.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
 abstract class WeatherRemoteDataSource {
-  Future<WeatherResponse> fetchSimpleWeather();
-  Future<WeatherResponse> fetchThrowsWeather({required String area});
   Future<WeatherResponse> fetchWeather({required WeatherRequest request});
 }
 
@@ -16,22 +14,6 @@ class WeatherRemoteDataSourceImpl implements WeatherRemoteDataSource {
   }) : _yumemiWeather = yumemiWeather;
 
   final YumemiWeather _yumemiWeather;
-
-  @override
-  Future<WeatherResponse> fetchSimpleWeather() async {
-    final response = _yumemiWeather.fetchSimpleWeather();
-    final jsonObject = jsonDecode(response) as Map<String, dynamic>;
-    return WeatherResponse.fromJson(jsonObject);
-  }
-
-  @override
-  Future<WeatherResponse> fetchThrowsWeather({
-    required String area,
-  }) async {
-    final response = _yumemiWeather.fetchThrowsWeather(area);
-    final jsonObject = jsonDecode(response) as Map<String, dynamic>;
-    return WeatherResponse.fromJson(jsonObject);
-  }
 
   @override
   Future<WeatherResponse> fetchWeather({
