bd9ca7187a77f37daedd966276c496818252bff6
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2023-12-12 11:22:45 +0900

    issue #9 更新: WeatherPageStateProvider を追加する

---
+++ lib/presentation/weather/weather_page_state_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather_page_state_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$weatherPageStateHash() => r'97de3eb29a276c35a6a5251db82a89b18c9faee3';
+
+/// See also [WeatherPageState].
+@ProviderFor(WeatherPageState)
+final weatherPageStateProvider =
+    AutoDisposeNotifierProvider<WeatherPageState, WeatherUiState>.internal(
+  WeatherPageState.new,
+  name: r'weatherPageStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$weatherPageStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$WeatherPageState = AutoDisposeNotifier<WeatherUiState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
