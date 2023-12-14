6952e0ca51877a0e6a91493ad78edd8330f2eaaf
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2023-12-14 18:41:24 +0900

    issue #10 修正: 時刻の単体テスト実行時に実行環境に依存する localize から utc で確認するように修正する

--- test/data/model/weather_request_test.dart
+++ test/data/model/weather_request_test.dart
@@ -9,17 +9,17 @@ void main() {
       '正常なWeatherRequestをJSONにエンコードする場合_正常なJSONを返却する',
       () {
         // arrange
-        const dateString = '2020-04-01T12:00:00+09:00';
+        const dateString = '2020-04-01T12:00:00';
         final weatherRequest = WeatherRequest(
           area: 'Tokyo',
-          date: DateTime.parse(dateString).toLocal(),
+          date: DateTime.parse(dateString).toUtc(),
         );
 
         // act
         final actual = jsonEncode(weatherRequest.toJson());
 
         // assert
-        const expected = '{"area":"Tokyo","date":"2020-04-01T12:00:00.000"}';
+        const expected = '{"area":"Tokyo","date":"2020-04-01T03:00:00.000Z"}';
         expect(actual, expected);
       },
     );
